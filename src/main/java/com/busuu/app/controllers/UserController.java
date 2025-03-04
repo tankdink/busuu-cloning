d11e6cdcb7823e700e7622c933230c0c62b9960e
Jully-02 <nguyenducthiep16@gmail.com>
2025-03-04 09:35:27 +0700

    feat: Implement user register, login, and CRUD role APIs

--- src/main/java/com/busuu/app/controllers/UserController.java
+++ src/main/java/com/busuu/app/controllers/UserController.java
@@ -2,37 +2,43 @@ package com.busuu.app.controllers;
 
 import com.busuu.app.configs.constant.Constants;
 import com.busuu.app.dtos.requests.user.UserDTO;
+import com.busuu.app.dtos.requests.user.UserLoginDTO;
+import com.busuu.app.dtos.responses.LoginResponse;
 import com.busuu.app.dtos.responses.Response;
+import com.busuu.app.dtos.responses.UserResponse;
+import com.busuu.app.entities.Token;
+import com.busuu.app.entities.User;
 import com.busuu.app.exceptions.ErrorHandleException;
+import com.busuu.app.services.token.ITokenService;
 import com.busuu.app.services.user.IUserService;
 import com.busuu.app.utils.LocalizationUtils;
 import com.busuu.app.utils.MessagesKey;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.UUID;
 
-@Controller
-@RestController(Constants.USER)
+@RestController
+@RequestMapping(Constants.USER)
 @RequiredArgsConstructor
 @Slf4j
 public class UserController {
     private final IUserService userService;
+    private final ITokenService tokenService;
     private final LocalizationUtils localizationUtils;
 
-    @PostMapping()
-    public ResponseEntity<Response> createUser (@RequestParam(value = "req-id", required = false) String requestId,
+    @PostMapping(value = Constants.REGISTER)
+    public ResponseEntity<Response> register (@RequestParam(value = "req-id", required = false) String requestId,
                                                 @Valid @RequestBody UserDTO userDTO,
                                                 BindingResult result) {
         try {
@@ -56,12 +62,79 @@ public class UserController {
                                 .build()
                 );
             }
-
-            return null;
+            UserResponse userResponse = userService.register(requestId, userDTO);
+            return ResponseEntity.ok(
+                    Response.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessagesKey.REGISTER_SUCCESSFULLY))
+                            .data(userResponse)
+                            .status(HttpStatus.CREATED)
+                            .build()
+            );
         } catch (Exception e) {
             log.error("Error when create user, " + e.getMessage());
-            throw new ErrorHandleException("Error when create user, " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR,
-                    Constants.ERROR_CODE.ERR_CREATE_USER, requestId);
+            return ResponseEntity.badRequest().body(
+                    Response.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessagesKey.REGISTER_FAILED))
+                            .status(HttpStatus.BAD_REQUEST)
+                            .build()
+            );
+        }
+    }
+
+    @PostMapping(value = Constants.LOGIN)
+    public ResponseEntity<Response> login (HttpServletRequest request,
+                                           @RequestParam(value = "req-id", required = false) String requestId,
+                                           @Valid @RequestBody UserLoginDTO userLoginDTO,
+                                           BindingResult result) {
+        try {
+            if (requestId == null || requestId.isEmpty()) {
+                requestId = UUID.randomUUID().toString();
+            }
+
+            if (result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors().stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+
+                // Log error
+                log.error(localizationUtils.getLocalizedMessage(MessagesKey.INVALID_ERROR, errorMessages.toString()));
+
+                return ResponseEntity.badRequest().body(
+                        Response.builder()
+                                .message(localizationUtils.getLocalizedMessage(MessagesKey.INVALID_ERROR, errorMessages.toString()))
+                                .status(HttpStatus.BAD_REQUEST)
+                                .build()
+                );
+            }
+
+            String token = userService.login(requestId, userLoginDTO.getEmail(), userLoginDTO.getPassword());
+            String userAgent = request.getHeader("User-Agent");
+            User user = userService.getUserDetailsFromToken(requestId, token);
+            Token jwtToken = tokenService.addToken(user, token, userAgent.toLowerCase().contains("mobile"));
+
+            LoginResponse loginResponse = LoginResponse.builder()
+                    .id(user.getId())
+                    .token(token)
+                    .tokenType(jwtToken.getTokenType())
+                    .refreshToken(jwtToken.getRefreshToken())
+                    .username(user.getUsername())
+                    .roles(user.getAuthorities().stream().map(GrantedAuthority::getAuthority).toList())
+                    .build();
+
+            return ResponseEntity.ok(
+                    Response.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessagesKey.LOGIN_SUCCESSFULLY))
+                            .data(loginResponse)
+                            .status(HttpStatus.OK)
+                            .build()
+            );
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(
+                    Response.builder()
+                            .message(localizationUtils.getLocalizedMessage(MessagesKey.LOGIN_FAILED))
+                            .status(HttpStatus.UNAUTHORIZED)
+                            .build()
+            );
         }
     }
 
--- src/main/java/com/busuu/app/services/user/UserService.java
+++ src/main/java/com/busuu/app/services/user/UserService.java
@@ -1,17 +1,32 @@
 package com.busuu.app.services.user;
 
+import com.busuu.app.components.JwtTokenUtil;
 import com.busuu.app.configs.constant.Constants;
 import com.busuu.app.dtos.requests.user.UserDTO;
 import com.busuu.app.dtos.requests.user.UserUpdateDTO;
 import com.busuu.app.dtos.responses.UserResponse;
+import com.busuu.app.entities.Role;
 import com.busuu.app.entities.User;
+import com.busuu.app.exceptions.DataNotFoundException;
 import com.busuu.app.exceptions.ErrorHandleException;
+import com.busuu.app.exceptions.PermissionDenyException;
+import com.busuu.app.repositories.RoleRepository;
 import com.busuu.app.repositories.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 @RequiredArgsConstructor
@@ -19,37 +34,105 @@ import org.springframework.stereotype.Service;
 public class UserService implements IUserService {
 
     private final UserRepository userRepository;
+    private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
     private final ModelMapper modelMapper;
 
     @Override
-    public UserResponse insertUser(String requestId, UserDTO userDTO) throws Exception {
+    @Transactional
+    public UserResponse register (String requestId, UserDTO userDTO) throws Exception {
         try {
-            return null;
+            // Check email
+            if (userRepository.existsByEmail(userDTO.getEmail())) {
+                throw new DataIntegrityViolationException("Email already exists");
+            }
+            List<Role> roles = userDTO.getRoleIds().stream()
+                    .map(roleId -> {
+                        try {
+                            Role role = roleRepository.findById(roleId)
+                                    .orElseThrow(() -> new DataNotFoundException("Cannot find Role with ID = " + roleId));
+                            if (role.getName().equalsIgnoreCase("ADMIN")) {
+                                throw new PermissionDenyException("You can not register a admin account");
+                            }
+                            return role;
+                        } catch (DataNotFoundException | PermissionDenyException e) {
+                            throw new RuntimeException(e);
+                        }
+                    })
+                    .toList();
 
+            User newUser = modelMapper.map(userDTO, User.class);
+            newUser.setId(UUID.randomUUID().toString());
+
+            String password = userDTO.getPassword();
+            String encodedPassword = passwordEncoder.encode(password);
+            newUser.setPassword(encodedPassword);
+
+            newUser.setRoles(roles);
+
+            // Active code to active account
+            newUser.setActiveCode(UUID.randomUUID().toString());
+            newUser.setActive(true);
+
+            // Save new user to db
+            userRepository.save(newUser);
+
+            // Send email active account here!
+
+            return modelMapper.map(newUser, UserResponse.class);
         } catch (Exception e) {
-            log.error("requestId="+requestId+",failed to create user, err="+e.getMessage());
-            throw new ErrorHandleException(e.getMessage(), HttpStatus.NOT_FOUND,
-                    Constants.ERROR_CODE.ERR_CREATE_USER, requestId);
+            log.error("requestId="+requestId+",failed to register user, err="+e.getMessage());
+            throw new ErrorHandleException(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR,
+                    Constants.ERROR_CODE.ERR_REGISTER_USER, requestId);
         }
     }
 
     @Override
-    public String login(String email, String password) throws Exception {
-        return null;
+    public String login(String requestId, String email, String password) throws Exception {
+        Optional<User> optionalUser = userRepository.findByEmail(email);
+        if(optionalUser.isEmpty()) {
+            throw new DataNotFoundException("Invalid email or password");
+        }
+        User existingUser= optionalUser.get();
+
+        // Check password
+        if (!passwordEncoder.matches(password, existingUser.getPassword())) {
+            throw new BadCredentialsException("Wrong email or password");
+        }
+
+        // Authenticate with Java Spring Security
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(
+                email, password,
+                existingUser.getAuthorities()
+        );
+        authenticationManager.authenticate(authenticationToken);
+        return jwtTokenUtil.generateToken(existingUser);
     }
 
     @Override
-    public boolean emailUnique(String email) {
-        return false;
+    public boolean emailUnique(String requestId, String email) {
+        return userRepository.existsByEmail(email);
     }
 
     @Override
-    public User getUserDetailsFromToken(String token) throws Exception {
-        return null;
+    public User getUserDetailsFromToken(String requestId, String token) throws Exception {
+        if (jwtTokenUtil.isTokenExpired(token)) {
+            throw new Exception("Token is expired");
+        }
+        String email = jwtTokenUtil.extractEmail(token);
+        Optional<User> user = userRepository.findByEmail(email);
+        if (user.isPresent()) {
+            return user.get();
+        }
+        else {
+            throw new Exception("User not found");
+        }
     }
 
     @Override
-    public UserResponse updateUser(String userId, UserUpdateDTO userUpdateDTO) throws Exception {
+    public UserResponse updateUser(String requestId, String userId, UserUpdateDTO userUpdateDTO) throws Exception {
         return null;
     }
 }
