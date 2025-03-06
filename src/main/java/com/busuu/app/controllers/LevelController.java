f7a178323d5ff01192efe718a31dc1e1e8b5838f
tankdink <tandinh143@gmail.com>
2025-03-06 12:14:44 +0700

    Remove redundant annotaiton

--- src/main/java/com/busuu/app/controllers/LevelController.java
+++ src/main/java/com/busuu/app/controllers/LevelController.java
@@ -19,7 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.UUID;
 
-@Controller
+
 @RestController
 @RequestMapping(Constants.LEVEL)
 @RequiredArgsConstructor
