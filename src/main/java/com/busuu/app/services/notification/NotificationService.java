7ee5a4258512f731cff06003c455824a2b94a946
tankdink <tandinh143@gmail.com>
2025-09-23 22:52:39 +0700

    Fix correction notification logic (2)

--- src/main/java/com/busuu/app/services/notification/NotificationService.java
+++ src/main/java/com/busuu/app/services/notification/NotificationService.java
@@ -126,7 +126,9 @@ public class NotificationService implements INotificationService
                     Correction destinationCorrection = correctionRepository.findById(destinationId)
                              .orElseThrow( ()-> new DataNotFoundException("Cannot find correction with ID"));
 
-                    destinationId = destinationCorrection.getCorrection().getPost().getId();
+                    if (destinationCorrection.getPost() != null) destinationId = destinationCorrection.getPost().getId();
+                    else destinationId = destinationCorrection.getCorrection().getPost().getId();
+
 
                     message = " has replied your correction";
 
